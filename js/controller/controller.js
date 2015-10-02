9340f19b732d0588f801d35bd325f88da7679595
Simon Jespersen <simjes91@me.com>
2015-10-02 16:59:01 +0200

    diff cur projects and old projects

--- js/controller/controller.js
+++ js/controller/controller.js
@@ -2,7 +2,7 @@ angular.module('simjesPortfolio.controllers', [])
     .controller('portCtrl', function($scope, $http) {
             $scope.getContent = function () {
                 $http.get('projects.json').then(function (result) {
-                    $scope.oldProjects = angular.fromJson(result).data;
+                    $scope.projects = angular.fromJson(result).data;
                     //alert("siste: " + JSON.stringify(result.data));
                 }, function (error) {
                     alert('error: ' + error.toString());
