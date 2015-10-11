cff21eeaa56e754b16cb59cab0ee6cccae8fd859
Simon Jespersen <s.jespersen@stud.uis.no>
2015-10-11 17:15:54 +0200

    added links

--- js/controller/controller.js
+++ js/controller/controller.js
@@ -1,11 +1,21 @@
 angular.module('simjesPortfolio.controllers', [])
     .controller('portCtrl', function ($scope, $http) {
+        $scope.counter = 0;
+
         $scope.getContent = function () {
             $http.get('projects.json').then(function (result) {
                 $scope.projects = angular.fromJson(result).data;
             }, function (error) {
                 alert('error: ' + error.toString());
             });
+        };
+
+        $scope.count = function () {
+            $scope.counter++;
+        };
+
+        $scope.resetCounter = function () {
+            $scope.counter = 0;
         };
 
     });
